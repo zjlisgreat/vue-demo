e7c1a9887067c351545df916cb98b22ff61b19cd
zjlisgreat <2191669006@qq.com>
2019-08-11 05:18:10 +0800

    axios+mock

--- src/components/secondcomponent.vue
+++ src/components/secondcomponent.vue
@@ -1,18 +1,64 @@
+<style>
+li{
+  list-style: none;
+}
+img{
+  width: 100px;
+  height: 100px;
+}
+.title{
+  display: inline-block;
+  width: 200px;
+  height: 35px;
+  line-height: 35px;
+  font-size: 14px;
+  text-align: center;
+  vertical-align: top;
+  font-weight: bold
+}
+p span{
+  font-size: 12px;
+  color: gray;
+  text-align: right
+}
+</style>
+
 <template>
     <div id="secondcomponent">
-        <h1>我是另外一个页面</h1>
+        <!-- <h1>我是另外一个页面</h1>
         <a href="">作者：{{ author }}</a>
-        <p>感谢大家的关怀<a href="https://github.com/">我的github</a></p>
+        <p>感谢大家的关怀<a href="https://github.com/">我的github</a></p> -->
+        <ul>
+          <li v-for="(item,index) in articles" :key="index">
+            <span class="title">{{ item.title }}</span>
+            <img :src="item.thumbnail_pic_s">
+            <p><span>作者:{{item.author_name}}</span>  <span>发表于:{{item.date}}</span></p>
+            <hr>
+          </li>
+        </ul>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data () {
       return {
         author: 'zjl',
         articles: []
       }
+  },
+  mounted () {
+    this.loadData()
+  },
+  methods: {
+    loadData: function () {
+      axios.post('/news').then((res) => {
+        this.articles = res.data.data
+        console.log(this.articles)
+      })
+    }
   }
 }
 </script>
---
+++ src/mock/mock.js
@@ -0,0 +1,28 @@
+const Mock = require('mockjs')
+// var data = Mock.mock({
+//   'list|1-10':[{
+//     'id|+1': 1
+//   }]
+// })
+
+// console.log(JSON.stringify(data, null, 4))
+
+const Random = Mock.Random
+const produceData = function (opt) {
+  console.log('opt', opt)
+  let articles = []
+  for (let i = 0; i < 30; i++) {
+    let newArticleObject = {
+      title: Random.csentence(5, 30),
+      thumbnail_pic_s: Random.dataImage('300x250', 'mock的图片'),
+      author_name: Random.cname(),
+      date: Random.date() + ' ' + Random.time()
+    }
+    articles.push(newArticleObject)
+  }
+  return {
+    data: articles
+  }
+}
+
+Mock.mock('/news', /post|get/i, produceData)
